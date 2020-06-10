fca2cf1fa49fe4367338f8f8e1fa067add20ff70
spurgiuss <33638065+spurgiuss@users.noreply.github.com>
2020-06-11 01:41:46 +0200

    Add files via upload

---
+++ Final/Drivers/AllDrivers.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final.Drivers
+{
+    public class AllDrivers
+    {
+        public static IWebDriver GetChromeDriver()
+        {
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4);
+            return driver;
+        }
+        public static IWebDriver GetFirefoxDriver()
+        {
+            var driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+
+            return driver;
+        }
+        public static IWebDriver GetIEDriver()
+        {
+            var driver = new InternetExplorerDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+
+            return driver;
+        }
+    }
+}
+
